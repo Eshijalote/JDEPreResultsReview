4abcb31373fdb9ee3f7d92397ca62ec66afd81f2
Eshi <eshijalote@gmail.com>
2020-01-21 15:18:05 -0600

    inital

---
+++ scripts/theme.js
@@ -0,0 +1,29 @@
+$(function() {
+
+	$('.header .navbar-nav a').smoothScroll();
+});
+
+
+function scrollTo(elem) {
+  $('body,html').animate({
+    scrollTop: elem.offset().top
+  }, 500);
+}
+
+$(function() {
+  $('#jump2top').css('bottom', '-100px');
+  $(window).scroll(function () {
+    var btn = $('#jump2top');
+    if ($(this).scrollTop() > 100) {
+      btn.stop().animate({'bottom' : '0'}, 200);
+    } else {
+      btn.stop().animate({'bottom' : '-100px'}, 200);
+  	}
+  });
+
+  $('#jump2top').smoothScroll();
+});
+
+
+
+
